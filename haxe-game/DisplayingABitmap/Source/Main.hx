4fa527b4d45dbf9fe470a48d14179c262187d899
Nycole <nycole.xavierr@gmail.com>
2021-06-28 13:01:41 -0300

    import

--- haxe-game/DisplayingABitmap/Source/Main.hx
+++ haxe-game/DisplayingABitmap/Source/Main.hx
@@ -5,6 +5,8 @@ import openfl.display.Bitmap;
 import openfl.display.BitmapData;
 import openfl.display.Sprite;
 import openfl.Assets;
+import openfl.events.Event;
+import openfl.Lib;
 
 
 class Main extends Sprite 
